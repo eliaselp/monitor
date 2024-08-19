3e88d9cbe324e798e11d39f5863bd0f6a51be4eb
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-08-18 18:32:36 -0700

    Add files via upload

---
+++ monitor/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from App import views
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('',views.Index.as_view()),
+    path('update/<str:data>/',views.Update.as_view()),
+    path('update_text_code/<str:data>/',views.update_text_code.as_view()),
+]
