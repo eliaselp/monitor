e71a10caf049fe25aaf92764a20872c4acf60597
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-08-23 13:39:36 -0700

    Add files via upload

--- monitor/urls.py
+++ monitor/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',views.Index.as_view()),
     path('update/<str:data>/',views.Update.as_view()),
     path('update_text_code/<str:data>/',views.update_text_code.as_view()),
+    path('reset_data/',views.reset.as_view()),
 ]
