13ee35f73573632d054efc0d4637e210e99a11f6
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-08-25 20:13:18 -0700

    Update views.py

--- App/views.py
+++ App/views.py
@@ -10,7 +10,7 @@ class Index(View):
     def get(self,request):
         text_code = None
         try:
-            if not models.Metadata.objects.filter(key="text_code").exists():
+            if models.Metadata.objects.filter(key="text_code").exists():
                 text_code = models.Metadata.objects.get(key="text_code")
                 text_code = text_code.texto
         except Exception as e:
